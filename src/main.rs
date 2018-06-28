3fa71e975d178b04442ec5adb12eb2b6df8fa722
Daniel Repp <daniel.g.repp@gmail.com>
2018-06-28 14:27:56 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+	loop {}
+}
+
