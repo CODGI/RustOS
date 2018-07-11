afb584d52bc0ebc819f2e5e98a87d72e6d607f24
Daniel Repp <daniel.g.repp@gmail.com>
2018-07-11 09:21:36 +0200

    Added support for cargo test, but bootimage throws an error when started with qemu

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![feature(panic_implementation)]
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
@@ -11,18 +12,17 @@ pub fn panic(info: &PanicInfo) -> ! {
 }
 
 
-
-
-#[no_mangle]
-#[macro_use]
-mod vga_buffer;
-pub extern "C" fn _start() -> ! {
-	println!("Hello World{}","!");	
-	loop{}
-}
-
 extern crate volatile;
 
 #[macro_use]
 extern crate lazy_static;
 extern crate spin;
+
+#[cfg(not(test))]
+#[no_mangle]
+#[macro_use]
+mod vga_buffer;
+pub extern "C" fn _start() -> ! {
+	loop{}
+}
+
